1cf2fb8d03aa2436bade6d0ba62c5ea478f1d018
Daweihao84 <dwsamurai84@gmail.com>
2023-12-02 01:05:34 +0800

    2023/12/2: 1. PhotoViewController's toolBar fixed!!! 2. Add the alertController in PhotoVC for delete button is ok!

---
+++ HW40_ImageEditor/ViewController/WhateverViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WhateverViewController.swift
+//  HW40_ImageEditor
+//
+//  Created by Dawei Hao on 2023/12/1.
+//
+
+import UIKit
+
+class WhateverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
